30ff62d46e6b8186d2b2a3ec5617ebbfbaaf066b
Ray Wu <yan.wu@stannah.co.uk>
2023-04-19 10:12:58 +0100

    #1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
-fn median(a: Vec<f32>) -> Option<f32> {
-    todo!();
+fn median(mut a: Vec<f32>) -> Option<f32> {
+    if a.is_empty() {
+        return None;
+    }
+    a.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    let length = a.len();
+    let middle = length / 2;
+    let med = if !is_even_length(length) {
+        a[middle]
+    } else {
+        avg(a[middle - 1], a[middle])
+    };
+    Some(med)
+}
+
+fn avg(a: f32, b: f32) -> f32 {
+    (a + b) / 2.0
+}
+
+fn is_even_length(len: usize) -> bool {
+    len % 2 == 0
 }
 
 fn main() {
     let answer = median(vec![1.0, 2.0, 5.0]);
 
     println!("median([1,2,5]) = {:?}", answer);
+}
+
+#[test]
+fn avg_test() {
+    let result = avg(4.0, 4.0);
+    assert_eq!(result, 4.0);
 }
 
 #[test]
