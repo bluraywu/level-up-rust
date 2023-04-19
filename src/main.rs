48f0c718855c9d742a6c5a0617ef2493978be264
Ray Wu <yan.wu@stannah.co.uk>
2023-04-19 11:12:31 +0100

    #2 solution

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
-fn unique(a: Vec<i32>) -> Vec<i32> {
-    todo!()
+//advanced 1: use generic types
+fn unique<T: Ord>(mut a: Vec<T>) -> Vec<T> {
+    a.sort_unstable();
+    a.dedup();
+    a
 }
-
-// advanced 1: use generic types
-// fn unique(a: Vec<T>) -> Vec<T> {
-//     todo!();
-// }
 
 // advanced 2: keep items in order
 // fn unique(a: Iterable<T>) -> Vec<T> {
 //     todo!();
 // }
 
-// advanced 3: use iterators
-// fn unique(a: Iterable<T>) -> Vec<T> {
-//     todo!();
+// use std::collections::HashSet;
+// use std::hash::Hash;
+//
+// // advanced 3: use iterators
+// fn unique<T: Ord + Hash>(a: Vec<T>) -> Vec<T> {
+//     let mut result = HashSet::<T>::new();
+//     for curr in a {
+//         result.insert(curr);
+//     }
+//     result.into_iter().collect()
 // }
 
 fn main() {
@@ -25,7 +30,7 @@ fn main() {
 
 #[test]
 fn empty_list() {
-    let input = vec![];
+    let input: Vec<i32> = vec![];
     let expected_output = vec![];
     let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
