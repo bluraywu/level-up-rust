1a975e53b3df9ffd8a2a53fd98f4cb890a9a849b
Ray Wu <yan.wu@stannah.co.uk>
2023-04-20 23:27:41 +0100

    #11 solution

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,42 @@ struct Isbn {
     digits: Vec<u8>,
 }
 
+#[derive(Debug)]
+enum MyError {
+    TooLong,
+    TooShort,
+    FailCheckSum,
+}
+
 impl FromStr for Isbn {
-    type Err = (); // TODO: replace with appropriate type
+    // TODO: replace with appropriate type
+    //too long
+    //too short
+    //failed checksum
+    type Err = MyError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!();        
+        let mut digits: Vec<u8> = vec![];
+        for c in s.chars() {
+            if c != '-' {
+                digits.push(c.to_digit(10).unwrap() as u8);
+            }
+        }
+        if digits.len() > 13 {
+            return Err(MyError::TooLong);
+        }
+        if digits.len() < 13 {
+            return Err(MyError::TooShort);
+        }
+        if digits[12] != calculate_check_digit(&digits[0..digits.len() - 1]) {
+            return Err(MyError::FailCheckSum);
+        }
+        Ok(
+            Self {
+                raw: s.to_string(),
+                digits,
+            }
+        )
     }
 }
 
@@ -21,7 +52,15 @@ impl std::fmt::Display for Isbn {
 
 // https://en.wikipedia.org/wiki/International_Standard_Book_Number#ISBN-13_check_digit_calculation
 fn calculate_check_digit(digits: &[u8]) -> u8 {
-    todo!()
+    let mut weight: u32 = 0;
+    for (idx, digit) in digits.iter().enumerate() {
+        weight += if idx % 2 == 0 {
+            *digit as u32
+        } else {
+            (digit * 3) as u32
+        };
+    }
+    ((10 - (weight % 10)) % 10) as u8
 }
 
 fn main() {
