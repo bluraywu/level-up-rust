3878eceb634a99efd042bc264c4efd83502b71f1
Ray <raywu1988@me.com>
2023-04-22 22:57:28 +0100

    #14 solution

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
 mod run_length_encoding {
     pub fn encode(text: &str) -> String {
-        todo!()
+        let mut curr_char = text.chars().nth(0).unwrap();
+        let mut curr_count = 0;
+        let mut result = String::new();
+
+        for (idx, c) in text.chars().enumerate() {
+            if curr_char != c {
+                result.push_str(&*curr_count.to_string());
+                result.push(curr_char);
+                curr_char = c;
+                curr_count = 1;
+            } else {
+                curr_count += 1;
+            }
+        }
+        result.push_str(&*curr_count.to_string());
+        result.push(curr_char);
+        result
     }
-    
+
     pub fn decode(text: &str) -> String {
-        todo!()
+        let mut digit_count = 0;
+        let mut result = String::new();
+        for (idx, c) in text.chars().enumerate() {
+            if c.is_alphabetic() {
+                let count: u32 = text[idx - digit_count..idx].parse().unwrap();
+                for _ in 0..count {
+                    result.push(c);
+                }
+                digit_count = 0;
+            } else {
+                digit_count += 1
+            }
+        }
+        result
     }
 }
 
@@ -33,5 +62,5 @@ fn long_run() {
     use run_length_encoding::*;
 
     let input = "AAAAA AAAAAAAAAA AAAAAAAAAAAAAAAAAAAA";
-    assert_eq!(encode(input), "5A1 9A1A1 9A9A2A");
+    assert_eq!(encode(input), "5A1 10A1 20A");
 }
